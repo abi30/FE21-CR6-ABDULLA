2bf54ea98b5800755cde605b2479de0feb034dab
Abdulla Al Rakib <36118180+abi30@users.noreply.github.com>
2021-03-27 01:55:44 +0100

    well done

--- myAppCR6/src/app/app-routing.module.ts
+++ myAppCR6/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/shared/header/header.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
 
-  {path:"",component:HeaderComponent}
+  // {path:"",component:HeaderComponent},
+  {path:"",component:NavComponent},
+
+  {path:"shoppingTravel",component:ShoppingCartComponent}
 ];
 
 @NgModule({
